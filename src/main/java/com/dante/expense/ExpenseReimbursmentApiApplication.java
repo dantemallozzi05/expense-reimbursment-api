19d29b715f226e2ae308da56e2694e87d1166578
dantemallozzi05 <dante.mallozzi@hotmail.com>
2026-02-04 08:57:31 -0500

    Spring Boot setup with Postgres, integrated health endpoint

---
+++ src/main/java/com/dante/expense/ExpenseReimbursmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.dante.expense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseReimbursmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseReimbursmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dante/expense/ExpenseReimbursmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dante.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseReimbursmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
